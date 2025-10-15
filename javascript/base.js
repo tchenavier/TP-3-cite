de6a89b0b6001bd81552f9554a394d458c028537
tchenavier <thibaud.chenavier@gmail.com>
2025-10-15 15:34:32 +0200

    Préparation pour javascript

---
+++ javascript/base.js
@@ -0,0 +1,2 @@
+//https://developer.mozilla.org/fr/docs/Web/JavaScript/Guide/Grammar_and_types#les_bases_du_langage
+//https://developer.mozilla.org/fr/docs/Web/HTML/How_to/Add_JavaScript_to_your_web_page
